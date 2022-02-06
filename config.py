6671b69ccb7c5ad4a809d3ffeabdc22d2f5f8e08
KidusAlemayehu <kidusalemayehu705@gmail.com>
2022-02-06 09:07:45 +0300

    first commit

--- config.py
+++ config.py
@@ -34,11 +34,8 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('EMAIL_SERVER')
-    MAIL_PORT = os.environ.get('EMAIL_PORT')
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_DEFAULT_SENDER')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
@@ -49,11 +46,8 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('EMAIL_SERVER')
-    MAIL_PORT = os.environ.get('EMAIL_PORT')
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_DEFAULT_SENDER')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
